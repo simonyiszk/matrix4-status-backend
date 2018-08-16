50b2d64574b76b6393f898867a43f6e7b2e96b7f
Nagy Simon <simon5521@sch.bme.hu>
2018-08-16 14:31:17 +0200

    First working code

--- matrix.command.tester/src/matrix/command/tester/Commander.java
+++ matrix.command.tester/src/matrix/command/tester/Commander.java
@@ -3,14 +3,28 @@ package matrix.command.tester;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 
 public class Commander {
 	
 	private byte commandByte;
 	private final int sendport=3000;
 	private final int recport=3001;
+
+	private DatagramSocket socket=null;
+	
+	public void closeSocket() {
+		socket.close();
+	}
 	
 	public Commander(String tipus) {
+
+		try {
+			socket=new DatagramSocket(recport);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		if(tipus.compareTo("12V-off-left")==0)
 			commandByte=0;
 		if(tipus.compareTo("12V-off-right")==0)
@@ -41,26 +55,24 @@ public class Commander {
 	}
 	
 	@SuppressWarnings("resource")
-	String send(String cel) throws Exception{
-		String valasz=null;
-		DatagramSocket socket=null;
+	byte[] send(String cel) throws Exception{
+		byte valasz[]=null;
 		DatagramPacket datagram=null;
 		byte[] buff=new byte[256];
 		
 		InetAddress cim;
-		cim=InetAddress.getByName(cel);
+		//cim=InetAddress.getByName(cel); 
+		cim=InetAddress.getByName("localhost"); //debuggolashoz
 		datagram=new DatagramPacket(toBytes(),4,cim,sendport);
 			
 		
 		
-		socket=new DatagramSocket(recport);
 		socket.send(datagram);
 		
 		DatagramPacket packet=new DatagramPacket(buff,buff.length);
 		socket.setSoTimeout(1000);
 		socket.receive(packet);
-		valasz=datagram.getData().toString();
-		socket.close();
+		valasz=datagram.getData();
 		return valasz;
 	}
 
--- matrix.command.tester/src/matrix/command/tester/Main.java
+++ matrix.command.tester/src/matrix/command/tester/Main.java
@@ -3,13 +3,14 @@ package matrix.command.tester;
 import java.io.IOException;
 
 public class Main {
+	static RestApiService service=null;
 
 	public static void main(String[] args) {
 		MuebArray cont=new MuebArray();
 		System.out.println(cont.toJsonString());
 		System.out.println("test started");
 		try {
-			RestApiService service=new RestApiService();
+			service=new RestApiService();
 			service.start();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -19,6 +20,15 @@ public class Main {
 		
 		Commander comm=new Commander("get-status");
 		System.out.println("initialisation is successful :)");
+
+		byte valasz1[];
+		try {
+			valasz1 = comm.send("localhost");
+			System.out.println(valasz1);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		/*for(int i=0;i<256;i++) {
 			System.out.println("sending to ip address: 192.168.1."+Integer.toString(i));
 			try {
@@ -35,10 +45,10 @@ public class Main {
 				for(Integer szoba=5;szoba<=12;szoba++) {
 					String ip="10.6."+emelet.toString()+"."+szoba.toString();
 					System.out.println("get status from ip address: "+ip);
-					Mueb m=cont.getMueb(emelet, szoba);
+					Mueb m=service.muebArray.getMueb(emelet, szoba);
 					try {
-						String valasz=comm.send(ip);
-						m.status=valasz;
+						byte valasz[]=comm.send(ip);
+						m.status=valasz.toString();
 						m.valid=true;
 						m.timeoutCount=0;
 					}catch(Exception e) {
@@ -46,8 +56,15 @@ public class Main {
 						m.timeoutCount++;
 						System.out.println(e.toString());
 					}
-					cont.setMueb(emelet, szoba, m);
+					service.muebArray.setMueb(emelet, szoba, m);
 				}
+			}
+
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 	}
